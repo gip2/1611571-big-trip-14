0b09e3301f5596bb6b172bc72a3715987232e7af
gip2 <gabitoff@yandex.ru>
2021-04-10 14:15:35 +0500

    Update main.js
    добавление вставки объектов событий путешествия в разметку SPA

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+const EVENT_NUM = 10;
+
 import {createMenuTemplate} from './view/menu.js';
 import {createInfoHeadTemplate} from './view/info-head.js';
 import {createFiltersTemplate} from './view/filters.js';
@@ -6,9 +8,17 @@ import {createTripEventTemplate} from './view/trip-event.js';
 import {createTripEventListTemplate} from './view/trip-event-list.js';
 import {createEditEventTemplate} from './view/edit-event.js';
 
+import {Event} from "./mock/travel.js";
+
 const render = (container, template, place = 'beforeend') => {
   container.insertAdjacentHTML(place, template);
 };
+
+const events = [];
+for (let index = 0; index < EVENT_NUM; index++) {
+  events.push(new Event());
+}
+// console.log(events);
 
 const siteTripMainElement = document.querySelector('.trip-main');
 const siteMenuElement = document.querySelector('#menu');
@@ -21,7 +31,16 @@ render(siteFiltersElement, createFiltersTemplate(), 'afterend');
 render(siteTripEventsHead, createSortTemplate(), 'afterend');
 render(siteTripEventsSection, createTripEventListTemplate());
 const siteTripEventsList = document.querySelector('.trip-events__list');
-render(siteTripEventsList, createEditEventTemplate());
-render(siteTripEventsList, createTripEventTemplate());
-render(siteTripEventsList, createTripEventTemplate());
-render(siteTripEventsList, createTripEventTemplate());
+render(siteTripEventsList, createEditEventTemplate(events[0]));
+
+events.forEach((element) => {
+  render(siteTripEventsList, createTripEventTemplate(element));
+});
+
+
+// import flatpickr from "flatpickr";
+// // Otherwise, selectors are also supported
+// flatpickr("#myID", {
+//   minDate: "today",
+//   maxDate: new Date().fp_incr(14) // 14 days from now
+// });
