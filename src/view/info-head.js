e379dd5f413bb36b43205dc7ae9a3056f064bea3
gip2 <gabitoff@yandex.ru>
2021-04-10 13:56:17 +0500

    Update info-head.js
    создание функции вычисления текста маршрута, даты и стоимости для заголовка

--- src/view/info-head.js
+++ src/view/info-head.js
@@ -1,3 +1,47 @@
+import {getMonthAndDay} from "./trip-event.js";
+const MAX_EVENTS_ITEMS = 3;
+
+const calculateInfoEvents = (events) => {
+  const info = {
+    title: ``,
+    date: ``,
+    cost: 0.0
+  };
+  if (!events || !events.length) {
+    return info;
+  }
+
+  if (events.length > MAX_EVENTS_ITEMS) {
+    info.title = `${events[0].destination}  &mdash; ... &mdash;  ${events[events.length - 1].destination}`;
+  } else {
+    info.title = events.map((event)=>event.destination).join(` &mdash;`);
+  }
+
+  let dateBegin = getMonthAndDay(events[0].dateBegin);
+  let dateEnd = getMonthAndDay(events[events.length - 1].dateEnd);
+  if (dateBegin === dateEnd) {
+    info.date = dateBegin;
+  } else {
+    if (dateBegin.slice(0, 3) === dateEnd.slice(0, 3)) {
+      info.date = `${dateBegin}&nbsp;&mdash;&nbsp;${dateEnd.slice(4)}`;
+    } else {
+      info.date = `${dateBegin}&nbsp;&mdash;&nbsp;${dateEnd}`;
+    }
+  }
+  info.cost = events.reduce((accumulator, event) => {
+    const {price, offers} = event;
+    accumulator += price;
+    if (offers !== undefined) {
+      accumulator = offers.reduce((acc, offer) => {
+        return acc + offer.checked ? offer.price : 0;
+      }, accumulator);
+    }
+    return accumulator;
+  }, 0);
+  return info;
+};
+
+
 export const createInfoHeadTemplate = () =>
   `<section class="trip-main__trip-info  trip-info">
     <div class="trip-info__main">
