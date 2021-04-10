7b9d16d7ebcd19cccd4a4852c51c3512e72d733c
gip2 <gabitoff@yandex.ru>
2021-04-10 14:07:14 +0500

    Update trip-event.js
    добавление в функцию отрисовки события вставки моковых данных: дат начала и завершения, пути иконки, текста пункта назначения, цены, списка опций.

--- src/view/trip-event.js
+++ src/view/trip-event.js
@@ -37,33 +37,31 @@ const createOffersListTemplate = (offers) => {
   return `<ul class="event__selected-offers">${liString}</ul>`;
 };
 
-export const createTripEventTemplate = () =>
-  `<div class="event">
-    <time class="event__date" datetime="2019-03-18">MAR 18</time>
+export const createTripEventTemplate = ({
+  dateBegin, typeIconSrc, typeText, destination, dateEnd, price, offers, favority
+}) => {
+  const favorityClass = favority ? `event__favorite-btn--active` : `event__favorite-btn`;
+
+  return `<div class="event">
+    <time class="event__date" datetime=${dateBegin}>${getMonthAndDay(dateBegin)}</time>
     <div class="event__type">
-      <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
+      <img class="event__type-icon" width="42" height="42" src="${typeIconSrc}" alt="Event type icon">
     </div>
-    <h3 class="event__title">Taxi Amsterdam</h3>
+    <h3 class="event__title">${typeText} ${destination}</h3>
     <div class="event__schedule">
       <p class="event__time">
-        <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
+        <time class="event__start-time" datetime="${dateBegin}">${getTime(dateBegin)}</time>
         —
-        <time class="event__end-time" datetime="2019-03-18T11:00">11:00</time>
+        <time class="event__end-time" datetime="${dateEnd}">${getTime(dateEnd)}</time>
       </p>
-      <p class="event__duration">30M</p>
+      <p class="event__duration">${durationTemplate(dateBegin, dateEnd)}</p>
     </div>
     <p class="event__price">
-      €&nbsp;<span class="event__price-value">20</span>
+      €&nbsp;<span class="event__price-value">${price}</span>
     </p>
     <h4 class="visually-hidden">Offers:</h4>
-    <ul class="event__selected-offers">
-      <li class="event__offer">
-        <span class="event__offer-title">Order Uber</span>
-        +€&nbsp;
-        <span class="event__offer-price">20</span>
-      </li>
-    </ul>
-    <button class="event__favorite-btn event__favorite-btn--active" type="button">
+    ${createOffersListTemplate(offers)}
+    <button class="event__favorite-btn ${favorityClass}" type="button">
       <span class="visually-hidden">Add to favorite</span>
       <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
         <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"></path>
@@ -73,3 +71,4 @@ export const createTripEventTemplate = () =>
       <span class="visually-hidden">Open event</span>
     </button>
   </div>`;
+};
