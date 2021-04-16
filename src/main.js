abc7d6902774c3f59c7c214e11f9c51732ec535b
gip2 <gabitoff@yandex.ru>
2021-04-16 10:14:08 +0500

    Update main.js
    добавлена функция отрисовки пункта маршрута в позицию перед концом списка маршрута с подфункциями сворачивания/разворачивания редактирования, отмены и подтверждения

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 const EVENT_NUM = 10;
 
-import {createMenuTemplate} from './view/controlBoard.js';
-import {createInfoHeadTemplate} from './view/info-head.js';
-import {createFiltersTemplate} from './view/filters.js';
-import {createSortTemplate} from './view/sort.js';
-import {createTripEventTemplate} from './view/trip-event.js';
-import {createTripEventListTemplate} from './view/trip-event-list.js';
-import {createEditEventTemplate} from './view/edit-event.js';
+import {ControlBoardView} from './view/controlBoard.js';
+import {createInfoHeadView} from './view/info-head.js';
+import {createFiltersView} from './view/filters.js';
+import {createSortView} from './view/sort.js';
+import {createTripEventView} from './view/trip-event.js';
+import {createTripEventListView} from './view/trip-event-list.js';
+import {createEditEventView} from './view/edit-event.js';
 import {Event} from './mock/travel.js';
 import {renderElement, RenderPosition} from "./utils.js";
 
@@ -21,16 +21,57 @@ const siteMenuElement = document.querySelector('#menu');
 const siteFiltersElement = document.querySelector('#filters');
 const siteTripEventsHead = document.querySelector('#tripEvents');
 const siteTripEventsSection = document.querySelector('.trip-events');
-render(siteTripMainElement, createInfoHeadTemplate(events), 'afterbegin');
-render(siteMenuElement, createMenuTemplate(), 'afterend');
-render(siteFiltersElement, createFiltersTemplate(), 'afterend');
-render(siteTripEventsHead, createSortTemplate(), 'afterend');
-render(siteTripEventsSection, createTripEventListTemplate());
+
+const renderTripEvent = (tripEventListElement, event) => {
+  const eventComponent = new TripEventView(event);
+  const eventEditComponent = new EditEventView(event);
+
+  const replaceEditToEvent = () => {
+    tripEventListElement.replaceChild(eventComponent.getElement(), eventEditComponent.getElement());
+  };
+
+  const replaceEventToEdit = () => {
+    tripEventListElement.replaceChild(eventEditComponent.getElement(), eventComponent.getElement());
+  };
+
+  const onEscKeyDown = (evt) => {
+    if (evt.key === KeyCode.ESCAPE || evt.key === KeyCode.ESC) {
+      evt.preventDefault();
+      replaceEditToEvent();
+      document.removeEventListener(`keydown`, onEscKeyDown);
+    }
+  };
+
+  eventComponent.getElement().querySelector(`.event__rollup-btn`).addEventListener(`click`, () => {
+    replaceEventToEdit();
+    document.addEventListener(`keydown`, onEscKeyDown);
+  });
+
+  eventEditComponent.getElement().querySelector(`.event__rollup-btn`).addEventListener(`click`, () => {
+    replaceEditToEvent();
+    document.removeEventListener(`keydown`, onEscKeyDown);
+  });
+
+  eventEditComponent.getElement().addEventListener(`submit`, (evt) => {
+    evt.preventDefault();
+    replaceEditToEvent();
+    document.removeEventListener(`keydown`, onEscKeyDown);
+  });
+
+  renderElement(tripEventListElement, eventComponent.getElement(), RenderPosition.BEFOREEND);
+};
+
+
+render(siteTripMainElement, createInfoHeadView(events), 'afterbegin');
+render(siteMenuElement, createMenuView(), 'afterend');
+render(siteFiltersElement, createFiltersView(), 'afterend');
+render(siteTripEventsHead, createSortView(), 'afterend');
+render(siteTripEventsSection, createTripEventListView());
 const siteTripEventsList = document.querySelector('.trip-events__list');
-render(siteTripEventsList, createEditEventTemplate(events[0]));
+render(siteTripEventsList, createEditEventView(events[0]));
 
 events.forEach((element) => {
-  render(siteTripEventsList, createTripEventTemplate(element));
+  render(siteTripEventsList, createTripEventView(element));
 });
 
 
